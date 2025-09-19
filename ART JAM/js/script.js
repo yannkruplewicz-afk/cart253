b553eb49cb726e09b91738dc448df91cb97c6cf8
yannkruplewicz-afk <yann.kruplewicz@gmail.com>
2025-09-19 17:19:33 -0400

    art jam hair and jacket done

--- ART JAM/js/script.js
+++ ART JAM/js/script.js
@@ -10,8 +10,12 @@
 /**
  * creates a canvas
 */
+
 function setup() {
-    createCanvas(1200, 1000);
+
+    createCanvas(1200, 1400);
+
+
 }
 
 
@@ -19,23 +23,35 @@ function setup() {
  * sets the background 
 */
 function draw() {
+
+
     background("#2D302E");
+
 
     DrawFrame();
 
+
+    DrawFace(); // draws the top of the head
+    DrawHair(); // draws the hair
+
     clip(frameMask, { invert: false });
+
 
     //head of frame
     push();
     fill("#22451D");
     noStroke();
     strokeWeight(2);
-    rect(50, 50, 1100, 1000);
+    rect(50, 50, 1100, 1400);
     pop();
+    //end of head of frame
+
 
     DrawJacket();   // draws the jacket step by step with different layers to create a 3D effect
     DrawHead(); // draws the head step by step with different layers to create a 3D effect
+    DrawFace(); // draws the top of the head
     DrawDetailsNeck(); // draws the details of the neck
+    DrawHair(); // draws the hair
 
 }
 
@@ -50,7 +66,7 @@ function DrawFrame() {
     noFill();
     stroke("grey");
     strokeWeight(20);
-    rect(50, 50, 1100, 900);
+    rect(50, 50, 1100, 1300);
     pop();
 
     // creates white borders of frame
@@ -58,7 +74,7 @@ function DrawFrame() {
     noFill();
     stroke("white");
     strokeWeight(10);
-    rect(50, 50, 1100, 900);
+    rect(50, 50, 1100, 1300);
     pop();
 
 }
@@ -68,6 +84,7 @@ function DrawFrame() {
 function DrawJacket() {
     //draws left bottom part of jacket
     push();
+    translate(0, 400); // Move it down because i changed the height of the canvas
     fill("#060809ff");
     noStroke();
     strokeWeight(2);
@@ -78,6 +95,7 @@ function DrawJacket() {
 
     //draws right bottom part of jacket
     push();
+    translate(0, 400); // Move it down because i changed the height of the canvas
     fill("#060809ff");
     noStroke();
     strokeWeight(2);
@@ -88,6 +106,7 @@ function DrawJacket() {
 
     //draws higher part of capuche of jacket
     push();
+    translate(0, 400); // Move it down because i changed the height of the canvas
     fill("#010206ff");
     noStroke();
     strokeWeight(2);
@@ -99,6 +118,7 @@ function DrawJacket() {
 
     //draws left part of capuche of jacket
     push();
+    translate(0, 400); // Move it down because i changed the height of the canvas
     fill("#010206ff");
     noStroke();
     strokeWeight(2);
@@ -109,6 +129,7 @@ function DrawJacket() {
 
     //draws right part of capuche of jacket
     push();
+    translate(0, 400); // Move it down because i changed the height of the canvas
     fill("#010206ff");
     noStroke();
     strokeWeight(2);
@@ -123,6 +144,7 @@ function DrawHead() {
 
     //layer of neck
     push();
+    translate(0, 400); // Move it down because i changed the height of the canvas
     fill("#FCC6BB");
     noStroke();
     translate(-435, 0); // Move the origin to the ellipse center
@@ -132,6 +154,7 @@ function DrawHead() {
     pop();
 
     push();
+    translate(0, 400); // Move it down because i changed the height of the canvas
     fill("#FCC6BB");
     noStroke();
     strokeWeight(2);
@@ -154,6 +177,7 @@ function DrawHead() {
 
 
     push();
+    translate(0, 400); // Move it down because i changed the height of the canvas
     fill("#D76D56");
     translate(510, -400); // creates a shadow behind the jaw
     rotate(radians(27)); // creates light brown shadow part 1 behind the jaw
@@ -162,6 +186,7 @@ function DrawHead() {
     pop();
 
     push();
+    translate(0, 400); // Move it down because i changed the height of the canvas
     fill("#d76d56");
     translate(310, -120); // creates a shadow behind the jaw
     rotate(radians(9)); // // creates light brown shadow part 2 behind the jaw
@@ -171,6 +196,7 @@ function DrawHead() {
 
     // creates big brown circle that acts as a shadow for the head 
     push();
+    translate(0, 400); // Move it down because i changed the height of the canvas
     fill("#8C2817");
     translate(50, 7); // Move the origin to the ellipse center
     rotate(radians(7)); // Rotate 
@@ -181,6 +207,7 @@ function DrawHead() {
 
     // imrpoves the jaw with another layer on the right side
     push();
+    translate(0, 400); // Move it down because i changed the height of the canvas
     fill("#FCC6BB");
     translate(49, 43); // Move the origin to the ellipse center
     rotate(radians(-14)); // Rotate 
@@ -190,6 +217,7 @@ function DrawHead() {
 
     // improves the jaw with another layer on the middle
     push();
+    translate(0, 400); // Move it down because i changed the height of the canvas
     fill("#FCC6BB");
     translate(-50, 50); // develops center of jaw
     rotate(radians(-4)); // Rotate 
@@ -197,41 +225,15 @@ function DrawHead() {
     ellipse(650, 700, 200, 50);
     pop();
 
-    // draws right ear
-    //right ear
-    push();
-    fill("#D76D56");
-    noStroke();
-    translate(100, -180); // Move the origin to the ellipse center
-    rotate(radians(12));
-    ellipse(990, 380, 90, 200);
-    pop();
-
-
-    push(); // head itself
-    fill("#FCC6BB");
-    noStroke();
-    ellipse(600, 300, 800, 850);
-    pop();
-
-
-    //left ear
-    push();
-    fill("#EB9383");
-    noStroke();
-    translate(240, 400); // Move the origin to the ellipse center
-    rotate(radians(-7)); // Rotate the left hair to have a good perspective
-    ellipse(0, 0, 190, 240); // Draw ellipse at the new origin
-    pop();
-
-
     push();   // develops right side of the jaw
+    translate(0, 400); // Move it down because i changed the height of the canvas
     fill("#FCC6BB");
     noStroke();
     translate(430, 1110);
     rotate(radians(-78));
     rect(600, 300, 190, 90);
     pop();
+    // end of jaw
 
 
 
@@ -243,6 +245,7 @@ function DrawDetailsNeck() {
 
     // creates circles of skin color in the neck, behind the jaw to improve the shadows
     push();
+    translate(0, 400); // Move it down because i changed the height of the canvas
     fill("#FFC6B8");
     translate(-66, -90); // develops center of jaw
     rotate(radians(4)); // Rotate 
@@ -258,6 +261,7 @@ function DrawDetailsNeck() {
 
     // develops center of jaw, the stroke creates the round thing the men have on the neck
     push();
+    translate(0, 400); // Move it down because i changed the height of the canvas
     fill("#eb9383");
     stroke("#d76d56");
     translate(-90, 17); // develops center of jaw, the stroke creates the round thing the men have on the neck
@@ -270,6 +274,172 @@ function DrawDetailsNeck() {
 
 
 }
+function DrawHair() {
+    // draws the hair
+    push();
+    fill("#2C1B0B");
+    noStroke();
+    ellipse(600, 265, 500, 100);
+    ellipse(640, 165, 300, 200);
+    translate(1100, -410); // right side of hair
+    rotate(radians(86));
+    ellipse(960, 165, 300, 60);
+    pop();
+
+    push();
+    fill("#24100B");
+    noStroke();
+    ellipse(600, 265, 170, 40);
+    ellipse(640, 165, 190, 20);
+    translate(1100, -410); // right side of hair
+    rotate(radians(86));
+    ellipse(960, 165, 100, 20);
+    pop();
+
+    push();
+    fill("#2C1B0B");
+    noStroke();
+    translate(530, -270); // right side of hair
+    rotate(radians(66));
+    ellipse(760, -100, 300, 90);
+    pop();
+
+    push();
+    fill("#24100B");
+    noStroke();
+    translate(530, -270); // right side of hair
+    rotate(radians(66));
+    ellipse(760, -100, 100, 30);
+    pop();
+
+    push();
+    fill("#2C1B0B");
+    noStroke();
+    translate(0, 0); // right side of hair
+    rotate(radians(0));
+    arc(790, 400, 300, 450, PI + QUARTER_PI, 0 + TWO_PI, OPEN);
+
+    push();// shadow of previous arc
+    fill("#24100B");
+    noStroke();
+    translate(0, 0); // right side of hair
+    rotate(radians(0));
+    arc(790, 330, 100, 150, PI + QUARTER_PI, 0 + TWO_PI, OPEN);
+
+
+    pop();
+
+
+    pop();
+    push();
+    fill("#2c1b0b");
+    noStroke();
+    translate(530, -270); // right side of hair
+    rotate(radians(66));
+    ellipse(870, -90, 120, 50);
+    pop();
+
+    push();
+    fill("#24100B");
+    noStroke();
+    translate(530, -270); // right side of hair
+    rotate(radians(66));
+    ellipse(870, -90, 40, 20);
+    pop();
+
+
+
+    pop();
+    push();
+    fill("#2C1B0B");
+    noStroke();
+    translate(-520, 380); // left side of hair
+    rotate(radians(-80));
+    ellipse(0, 795, 400, 150);
+
+    pop();
+
+    push();
+    fill("#24100B");
+    noStroke();
+    translate(-520, 380); // left side of hair
+    rotate(radians(-80));
+    ellipse(0, 765, 300, 100);
+    pop();
+
+    push();
+    fill("#2C1B0B");
+    noStroke();
+    translate(-20, -380); // left side of hair
+    rotate(radians(-30));
+    ellipse(0, 795, 300, 150);
+    ellipse(-40, 795, 300, 150);
+    ellipse(-80, 825, 300, 100);
+    ellipse(60, 745, 300, 100);
+
+    pop();
+
+    // shadows of the hair
+    push();
+    fill("#24100B");
+    noStroke();
+    translate(-20, -380); // left side of hair
+    rotate(radians(-30));
+    ellipse(0, 795, 200, 50);
+    ellipse(-40, 795, 100, 50);
+    ellipse(-80, 825, 100, 30);
+    ellipse(60, 745, 100, 30);
+
+
+    pop();
+
+
+}
+
+function DrawFace() {
+    // draws the face
+
+    // BEGGINNING OF EARS AND DRAWING OF THE FACE
+
+    // draws right ear
+    //right ear
+    push();
+    translate(0, 400); // Move it down because i changed the height of the canvas
+    fill("#D76D56");
+    noStroke();
+    translate(100, -180); // Move the origin to the ellipse center
+    rotate(radians(12));
+    ellipse(990, 380, 90, 200);
+    pop();
+
+
+    push(); // head itself
+    translate(0, 400); // Move it down because i changed the height of the canvas
+    fill("#FCC6BB");
+    noStroke();
+    ellipse(600, 300, 800, 850);
+    pop();
+
+    push(); // head itself
+    translate(0, 400); // Move it down because i changed the height of the canvas
+    fill("#FCC6BB");
+    noStroke();
+    ellipse(800, 100, 400, 450);
+    pop();
+
+
+    //left ear
+    push();
+    translate(0, 400); // Move it down because i changed the height of the canvas
+    fill("#EB9383");
+    noStroke();
+    translate(200, 400); // Move the origin to the ellipse center
+    rotate(radians(-9)); // Rotate the left hair to have a good perspective
+    ellipse(0, 0, 140, 240); // Draw ellipse at the new origin
+    pop();
+
+} // end of ears and drawing of the face
+
 
 
 

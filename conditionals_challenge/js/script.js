396b0f01f73447fbb39dd4ba9040cab5a2ace654
yannkruplewicz-afk <yann.kruplewicz@gmail.com>
2025-09-25 16:39:03 -0400

    conditionals challenge advancement

--- conditionals_challenge/js/script.js
+++ conditionals_challenge/js/script.js
@@ -1,16 +1,19 @@
 /**
- * Circle Master
- * Pippin Barr
+ * Conditionals challenge
+ * Yann Kruplewicz
  *
- * This will be a program in which the user can push a circle
- * on the canvas using their own circle.
+ *This is the beggining of the conditionals challenge !
  */
 
 const puck = {
     x: 200,
     y: 200,
     size: 100,
-    fill: "#ff0000"
+    fill: "#ff0000", // red to start
+    fills: {
+        noOverlap: "#ff0000", // red for no overlap
+        overlap: "#00ff00" // green for overlap
+    }
 };
 
 const user = {
@@ -23,6 +26,7 @@ const user = {
 /**
  * Create the canvas
  */
+
 function setup() {
     createCanvas(400, 400);
 }
@@ -39,6 +43,9 @@ function draw() {
     // Draw the user and puck
     drawUser();
     drawPuck();
+    movePuck()
+    drawTarget()
+
 }
 
 /**
@@ -69,4 +76,68 @@ function drawPuck() {
     fill(puck.fill);
     ellipse(puck.x, puck.y, puck.size);
     pop();
+}
+
+function movePuck() {
+
+    // Check overlap
+
+    // Calculate distance between circles' centres
+    const d = dist(user.x, user.y, puck.x, puck.y);
+    const overlap = (d < puck.size / 2 + user.size / 2);
+    // Check if that distance is smaller than their two radii, 
+    // because if it is, they are overlapping by the amazing
+    // power of geometry!
+
+    // Set fill based on whether they overlap
+    if (overlap) {
+        puck.fill = puck.fills.overlap;
+    }
+    else {
+        puck.fill = puck.fills.noOverlap
+
+    }
+
+
+}
+
+function drawTarget() {
+    push();
+    fill(target.fills.NO.big)
+    ellipse(target.x, target.y, target.size);
+    fill(target.fills.NO.small)
+    ellipse(target.x, target.y, target.size / 2);
+    pop();
+
+    const d = dist(target.x, target.y, puck.x, puck.y);
+    const overlap = (d < puck.size / 2 + target.size / 2);
+    // Check if that distance is smaller than their two radii, 
+    // because if it is, they are overlapping
+
+
+
+
+}
+
+let target = {
+    x: 150,
+    y: 50,
+    size: 50,
+    fills: {
+        NO: {
+            small: "#ff0000", // red for no overlap
+            big: "#fff" // green for overlap
+        }
+        //NO: {
+        // small: "#0000ff", // red for no overlap
+        //big: "#00ff00" // green for overlap
+    }
+}
+
+if (overlap) {
+    target.fills = target.fills.overlap;
+}
+else {
+    target.fills = target.fills.noOverlap
+
 }
